acf9f46c7a3ea7ee3d8af6e1fac23d7018b8d572
KohiiTM <jamesandallen.dev@gmail.com>
2025-02-02 23:42:49 -0800

    add mode toggle switch

---
+++ public/js/script.js
@@ -0,0 +1,19 @@
+const toggleSwitch = document.getElementById('theme-toggle');
+
+// Check for saved theme preference
+const currentTheme = localStorage.getItem('theme');
+if (currentTheme === 'dark') {
+  document.body.classList.add('dark-mode');
+  toggleSwitch.checked = true;
+}
+
+// Toggle Theme on Switch
+toggleSwitch.addEventListener('change', () => {
+  if (toggleSwitch.checked) {
+    document.body.classList.add('dark-mode');
+    localStorage.setItem('theme', 'dark');
+  } else {
+    document.body.classList.remove('dark-mode');
+    localStorage.setItem('theme', 'light');
+  }
+});
